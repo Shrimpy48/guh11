d79346552fd92d9540539439f3a0a965cb2febec
Jana <jscholey@protonmail.com>
2019-11-17 07:15:43 +0000

    fun patterns

--- algs.py
+++ algs.py
@@ -1,7 +1,7 @@
 algs = {
-    "superflip": "U R2 F B R B2 R U2 L B2 R U' D' R2 F R' L B2 U2 F2",
-    "checkerboard": "M2 E2 S2",
-    "cube_in_a_cube": "U' L' U' F' R2 B' R F U B2 U B' L U' F U R F'",
-    "python": "F2 R' B' U R' L F' L F' B D' R B L2",
-    "anaconda": "L U B' U' R L' B R' F B' D R D' F'"
+    "Superflip": "U R2 F B R B2 R U2 L B2 R U' D' R2 F R' L B2 U2 F2",
+    "Checkerboard": "M2 E2 S2",
+    "Cube_in_a_cube": "U' L' U' F' R2 B' R F U B2 U B' L U' F U R F'",
+    "Python": "F2 R' B' U R' L F' L F' B D' R B L2",
+    "Anaconda": "L U B' U' R L' B R' F B' D R D' F'"
 }
